a0a3373d5d05b91f03444af2cd2a6e9be4150f07
KsanaStankevich <98518922+KsanaStankevich@users.noreply.github.com>
2022-02-04 19:22:40 +0300

    HomeWork 10

---
+++ HW_16.cs
@@ -0,0 +1,10 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+int a = new Random().Next(1, 7);
+Console.WriteLine(a);
+
+if (a == 6 || a == 7)
+{
+    Console.WriteLine("Выходной");
+}
+else Console.WriteLine("Не выходной");
