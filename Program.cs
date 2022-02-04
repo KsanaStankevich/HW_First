f0c2e2d2ce45e4114d70bc7c1272e39e05553cc8
KsanaStankevich <98518922+KsanaStankevich@users.noreply.github.com>
2022-02-04 19:16:08 +0300

    HomeWork 10

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать вторую цифру трёхзначного числа.
+
+int a = new Random().Next(100, 999);
+Console.WriteLine(a);
+int Function(int b)
+{
+    return (b % 100)/10;
+}
+
+Console.WriteLine(Function(a));
