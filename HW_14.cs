fc6cbd7c101b87146ec5987aba5124d5483f4c4a
KsanaStankevich <98518922+KsanaStankevich@users.noreply.github.com>
2022-02-04 19:22:25 +0300

    HomeWork 14

---
+++ HW_14.cs
@@ -0,0 +1,14 @@
+﻿// Найти третью цифру числа или сообщить, что её нет.
+
+int a = new Random().Next(1, 99999);
+Console.WriteLine(a);
+
+if (a > 99)
+{
+    while (a > 999)
+    {
+        a = a/10;
+    }Console.WriteLine(a%10);
+}
+else Console.WriteLine("Третьей цифры нет");
+    
