8929f290260764cb3675197ffff9ac92ce26e223
KsanaStankevich <98518922+KsanaStankevich@users.noreply.github.com>
2022-02-04 19:16:38 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,10 +1,9 @@
-﻿// Показать вторую цифру трёхзначного числа.
+﻿// Удалить вторую цифру трёхзначного числа.
 
 int a = new Random().Next(100, 999);
 Console.WriteLine(a);
-int Function(int b)
-{
-    return (b % 100)/10;
-}
 
-Console.WriteLine(Function(a));
+int b = (a / 100)*10;
+int c = (a % 10);
+int result = b+c;
+Console.WriteLine(result);
