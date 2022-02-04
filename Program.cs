14bc1c6596d569863d9d08b0a043c905f283de20
KsanaStankevich <98518922+KsanaStankevich@users.noreply.github.com>
2022-02-04 19:21:28 +0300

    Delete

--- Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// Удалить вторую цифру трёхзначного числа.
-
-int a = new Random().Next(100, 999);
-Console.WriteLine(a);
-
-int b = (a / 100)*10;
-int c = (a % 10);
-int result = b+c;
-Console.WriteLine(result);
