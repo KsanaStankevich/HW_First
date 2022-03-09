70ccc2db8bac00b7996cc070f52f951e615e1c5f
KsanaStankevich <98518922+KsanaStankevich@users.noreply.github.com>
2022-03-09 11:56:46 +0300

    Add files via upload

---
+++ HW_57.cs
@@ -0,0 +1,51 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+void FillArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+void PrintArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+void Sort(int[,] arr)
+{
+    for (int x = 0; x < arr.GetLength(0); x++)
+    {
+        for (int p = 0; p < arr.GetLength(1); p++)
+        {
+            for (int y = 0; y < arr.GetLength(1) - 1; y++)
+            {
+                if (arr[x, y] > arr[x, y + 1])
+                {
+                    int z = arr[x, y];
+                    arr[x, y] = arr[x, y + 1];
+                    arr[x, y + 1] = z;
+                }
+            }
+        }
+    }
+}
+
+int m = 4;
+int n = 8;
+int[,] array = new int[m, n];
+
+FillArr(array);
+PrintArr(array);
+Console.WriteLine();
+Sort(array);
+PrintArr(array);
---
+++ HW_59.cs
@@ -0,0 +1,54 @@
+﻿// В прямоугольной матрице найти строку с наименьшей суммой элементов.
+
+void FillArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+void PrintArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = 5;
+int n = 5;
+int[,] array = new int[m, n];
+
+FillArr(array);
+PrintArr(array);
+Console.WriteLine();
+
+int result = 0;
+int i = 1;
+int number = 1;
+int a = 100000; // Для теста ввела рандомное большое число
+for (int x = 0; x < array.GetLength(0); x++)
+{
+    for (int y = 0; y < array.GetLength(1); y++)
+    {
+        result = result + array[x, y];
+    }
+    Console.WriteLine($"Сумма элементов по строке {i} равна {result}");
+
+    if (result < a)
+    {
+        number = i;
+        a = result;
+    }
+    result = 0;
+    i++;
+}
+Console.WriteLine();
+Console.WriteLine($"Cтрока с наименьшей суммой элементов: {number}");
