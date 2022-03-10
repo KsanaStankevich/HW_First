56481c50e5457e6f64f15a5c43e4d32ed8782663
KsanaStankevich <98518922+KsanaStankevich@users.noreply.github.com>
2022-03-10 13:22:54 +0300

    Add files via upload

---
+++ HW_67.cs
@@ -0,0 +1,13 @@
+﻿// Показать натуральные числа от N до 1, N задано.
+// N = 5. -> "5, 4, 3, 2, 1"
+
+void Func(int num1, int num2)
+{
+    Console.Write($"{num1} ");
+    if (num2 < num1)
+        Func(num1 - 1, num2);
+    else
+        Console.ReadKey();
+}
+
+Func(10, 1);
---
+++ HW_69.cs
@@ -0,0 +1,13 @@
+﻿//  Задайте значения M и N. Напишите программу, которая найдёт сумму элементов в промежутке от M до N.
+// M = 1; N = 4 -> 10
+// M = 4; N = 8. -> 30
+
+int Func(int x, int y)
+{
+    int s = 0;
+    if ((y - 1) == x) return s = y + x;
+    else s = y + Func(x, y - 1);
+    return (s);
+}
+
+Console.WriteLine(Func(1, 4));
